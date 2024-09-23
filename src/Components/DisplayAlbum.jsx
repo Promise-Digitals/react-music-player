41130bbd17807ec74891a7f95abd1f9df47516e6
Promise-Digitals <promiseedeh025@gmail.com>
2024-09-23 19:56:33 +0100

    Completed Display Album UI

--- src/Components/DisplayAlbum.jsx
+++ src/Components/DisplayAlbum.jsx
@@ -37,7 +37,16 @@ const DisplayAlbum = () => {
             <hr />
             {
                 songsData.map((item, index)=>(
-                    <div></div>
+                    <div className='grid grid-cols-3 sm:grid-cols-4 gap-2 p-2 items-center text-[#a7a7a7] hover:bg-[#ffffff2b] cursor-pointer' key={index}>
+                        <p className='text-white'>
+                            <b className='mr-4 text-[#a7a7a7]'>{index+1}</b>
+                            <img src={item.image} className='inline w-10 mr-5' />
+                            {item.name}
+                        </p>
+                        <p className='text-[15px]'>{albumData.name}</p>
+                        <p className='text-[15px] hidden sm:block'>5 days ago</p>
+                        <p className='text-[15px] text-center'>{item.duration}</p>
+                    </div>
                 ))
             }
         </>
